b85b0642bcf1546897e60abb22054be15ff6e984
Georgi Stoilov <gogo.stoilov@abv.bg>
2019-10-31 11:04:48 +0200

    Update task4.R

--- task4.R
+++ task4.R
@@ -13,4 +13,11 @@ task 3:
  a = rhyper(1000, 7, 6, 8)
  mean(a)
  var(a)
- 
+ barplot(table(a))
+ hist(a)
+ hist(a, breaks = 1:8
+ hist(a, breaks = 1:8, probability = T) - Емперично разпределение
+ dhyper(2:7, 7, 6, 8)
+ sum(dhyper(2:7, 7, 6, 8))
+ lines(c(1,2,2,3,3,4,4,5,5,6), x)
+  x = c(1,2,2,3,3,4,4,5,5,6,6,7) => Правим и y да е със стойности от hyper и ги match-ваме
