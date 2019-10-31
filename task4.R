6c133d9fd7e8df89a81eef4a834f5a8022ab44b7
Georgi Stoilov <gogo.stoilov@abv.bg>
2019-10-31 10:54:47 +0200

    Update task4.R

--- task4.R
+++ task4.R
@@ -1,3 +1,4 @@
+task 2
 dbinom(2, 7 , 0.2) * 0.2 2.a)
 
 pbinom(2, 6, 0.2) 2.b)
@@ -6,3 +7,10 @@ pbinom(2, 6, 0.2) 2.b)
 sum(dbinom(2, 4:7, 0.2)) * 0.2
 Или
 (dbinom(2, 4, 0.2) + dbinom(2, 5, 0.2) + dbinom(2, 6, 0.2) + dbinom(2, 7, 0.2))*0.2
+
+
+task 3:
+ a = rhyper(1000, 7, 6, 8)
+ mean(a)
+ var(a)
+ 
