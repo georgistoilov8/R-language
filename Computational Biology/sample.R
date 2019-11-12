df911c42392ea0b5003fcc3a16e23fb657299179
Georgi <gogo.stoilov@abv.bg>
2019-11-12 21:59:22 +0200

    Re-organize the repo

---
+++ Computational Biology/sample.R
@@ -0,0 +1,12 @@
+f = function(){
+  populationSize = 10000
+  selectionStrength = 0.001
+  frequency1 = 0.1
+  numberOfGenerations = 40000
+  frequencies = numeric(numberOfGenerations)
+  time = 1:numberOfGenerations  
+  for (i in time) {
+    print(i)
+  }
+}
+f
