7f4c7ceda749c125398aafd9d8ea223cce7164a6
Georgi Stoilov <gogo.stoilov@abv.bg>
2019-11-14 09:52:57 +0200

    Test if data is normalized

--- theory.R
+++ theory.R
@@ -21,3 +21,20 @@ plot(q, sort(x))
 
 qqnorm(x) -> ако са около една права, то данните са нормални, ако не са => не са нормални
 qqline(x)
+
+
+//
+shapiro.test(x)
+
+	Shapiro-Wilk normality test
+
+data:  x
+W = 0.98329, p-value = 0.2378
+Ако p-value е над 0.05, то приемаме, че данните са нормално разпределени
+shapiro.test(y)
+
+	Shapiro-Wilk normality test
+
+data:  y
+W = 0.93566, p-value = 0.0001058
+Ако p-value е под 0.05, то приемаме, че данните не са нормално разпределени
