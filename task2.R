306e6cde223f7ba0d3e63bab090de1143db96c37
Georgi Stoilov <gogo.stoilov@abv.bg>
2019-11-14 10:44:47 +0200

    Task 2, b)

--- task2.R
+++ task2.R
@@ -32,3 +32,66 @@ shapiro.test(c)
 
 data:  c
 W = 0.99058, p-value = 0.7115
+
+// Задач 2, подточка б)
+x = runif(100, 0, 6)
+m = matrix(x, nrow = 100)
+c = rowSums(m)
+qqnorm(c)
+shapiro.test(c)
+
+	Shapiro-Wilk normality test
+
+data:  c
+W = 0.9404, p-value = 0.0002039
+
+x = runif(200, 0, 6)
+m = matrix(x, nrow = 100)
+c = rowSums(m)
+qqnorm(c)
+shapiro.test(c)
+
+	Shapiro-Wilk normality test
+
+data:  c
+W = 0.98044, p-value = 0.1437
+
+ 
+x = runif(1000, 0, 6)
+m = matrix(x, nrow = 100)
+c = rowSums(m)
+qqnorm(c)
+shapiro.test(c)
+
+	Shapiro-Wilk normality test
+
+data:  c
+W = 0.98906, p-value = 0.5889
+
+
+x = runif(10000, 0, 6)
+m = matrix(x, nrow = 100)
+c = rowSums(m)
+qqnorm(c)
+shapiro.test(c)
+
+	Shapiro-Wilk normality test
+
+data:  c
+W = 0.98939, p-value = 0.6156
+
+
+x = runif(100000, 0, 6)
+m = matrix(x, nrow = 100)
+c = rowSums(m)
+qqnorm(c)
+shapiro.test(c)
+
+	Shapiro-Wilk normality test
+
+data:  c
+W = 0.98308, p-value = 0.2293
+
+hist(c)
+hist(c, probability = T)
+lines(density(c))
